7d90a42660586cab24a9fa0c14bf858ab10ca46d
Carlos <alcros_12@hotmail.com>
2015-06-06 02:29:33 -0500

    segundo commit

--- test.java
+++ test.java
@@ -2,6 +2,6 @@ public class test {
  	public static void main(String args[]){
   	  System.out.println("****************************");
   	  System.out.println("**Hola mundo, Hola jenkins**");
-  	  System.out.println("****************************");
+  	  System.out.println("Hola Hola Hola Hola");
 	 }
 }
