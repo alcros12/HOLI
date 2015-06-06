ada1eab52fba382b0235a88e6b319ccb7235285b
Carlos <alcros_12@hotmail.com>
2015-06-06 11:53:56 -0500

    gsdfs

--- test.java
+++ test.java
@@ -1,8 +1,5 @@
 public class test {
  	public static void main(String args[]){
   	  System.out.println("****************************");
-  	  System.out.println("**Hola mundo, Hola jenkins**");
-  	  System.out.println("Hola Hola Hola Hola");
-  	  System.out.println("GGGG");
 	 }
 }
